6b050ccebda77330e0b73369cdcb847b0fe93f40
EJLee1209 <dldmswo1209@gmail.com>
2023-10-27 16:59:21 +0900

    Feat: MainTabBarController 셋업

---
+++ KakaoClone/View/FriendListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FriendListViewController.swift
+//  KakaoClone
+//
+//  Created by 이은재 on 10/27/23.
+//
+
+import UIKit
+
+class FriendListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
